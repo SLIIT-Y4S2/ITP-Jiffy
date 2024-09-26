b4476c9c5c6f8802ea1feb8b1dc8327be3729787
IT20611088 <it20611088@my.sliit.lk>
2024-09-26 14:53:02 +0530

    NOSQL injection mitigation

--- server/controllers/supplierOrderController.js
+++ server/controllers/supplierOrderController.js
@@ -4,53 +4,53 @@ var sendEmail = require('../utils/sendEmail')
 
 //Create new order
 const createSupplierOrder = async (req, res) => {
-    const { supplierId, rawMaterial, quantity, unit} = req.body
+    const { supplierId, rawMaterial, quantity, unit } = req.body
 
     //adding to the db
-    try{
+    try {
         const order = await SupplierOrder.create({ supplierId, rawMaterial, quantity, unit })
         res.status(200).json(order)
-    }catch (error){
-        res.status(400).json({error:error.message})
+    } catch (error) {
+        res.status(400).json({ error: error.message })
     }
 }
 
 //Get all orders
 const getAllSupplierOrders = async (req, res) => {
-    const orders = await SupplierOrder.find({}).sort({createdAt: -1})
-    
+    const orders = await SupplierOrder.find({}).sort({ createdAt: -1 })
+
     res.status(200).json(orders)
 }
 
 //Get unpaid orders
 const getUnpaidSupplierOrders = async (req, res) => {
-    const orders = await SupplierOrder.find({orderStatus:"Pending"}).sort({createdAt: -1})
-    
+    const orders = await SupplierOrder.find({ orderStatus: "Pending" }).sort({ createdAt: -1 })
+
     res.status(200).json(orders)
 }
 
 
 //Get one orders
 
-const getOneSupplierOrder=async (req, res) =>{
+const getOneSupplierOrder = async (req, res) => {
 
     const order = await SupplierOrder.findById(req.params.id);
-    
+
     res.send(order);
 }
 
 //Delete an order
 const deleteSupplierOrder = async (req, res) => {
-    const {id} = req.params
+    const { id } = req.params
 
     if (!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(404).json({error: 'No such order'})
+        return res.status(404).json({ error: 'No such order' })
     }
 
-    const order = await SupplierOrder.findOneAndDelete({_id: id})
+    const order = await SupplierOrder.findOneAndDelete({ _id: id })
 
     if (!order) {
-        return res.status(400).json({error: 'No such order'})
+        return res.status(400).json({ error: 'No such order' })
     }
 
     res.status(200).json(order)
@@ -62,7 +62,7 @@ const updateSupplierOrder = async (req, res) => {
     const order = await SupplierOrder.findById(req.params.id);
 
     if (order) {
-        
+
         //order.deliveredAt = Date.now();
         order.orderStatus = "Received";
         order.Date = Date.now();
@@ -71,29 +71,29 @@ const updateSupplierOrder = async (req, res) => {
 
         res.json(updatedOrder);
     } else {
-      res.status(404);
-      throw new Error('Order not found');
+        res.status(404);
+        throw new Error('Order not found');
     }
-  
-    
+
+
 }
 
 
 const updateSupplierOrderPayment = async (req, res) => {
-    
+
     const payment = await SupplierOrder.findByIdAndUpdate(req.params.id
         ,
         {
-        supplierId:req.body.supplierId,
-         rawMaterial: req.body. rawMaterial,
-        quantity:req.body.quantity,
-        unit:req.body.unit,
-         orderStatus:req.body. orderStatus
+            supplierId: { $eq: req.body.supplierId },
+            rawMaterial: { $eq: req.body.rawMaterial },
+            quantity: { $eq: req.body.quantity },
+            unit: { $eq: req.body.unit },
+            orderStatus: { $eq: req.body.orderStatus }
         },
-        {new:true}
-        );
-        sendEmail(req.body.email, 'Payment completed', `We have paid for your ${req.body.rawMaterial}`)
-        res.send(payment);
+        { new: true }
+    );
+    sendEmail(req.body.email, 'Payment completed', `We have paid for your ${req.body.rawMaterial}`)
+    res.send(payment);
 };
 
 
@@ -108,7 +108,7 @@ module.exports = {
     updateSupplierOrder,
     updateSupplierOrderPayment,
     getUnpaidSupplierOrders
-    
-  
+
+
 
 }
